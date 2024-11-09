7d59d02440c3c8ac5e43c839e9835f550541a89b
Dirk Arnez <smalldirkalex@gmail.com>
2024-11-09 21:53:05 +0800

    - upload files

---
+++ composer-install.cmd
@@ -0,0 +1,4 @@
+%~dp0..\bin\php\php-8.0.0-Win32-vs16-x64\php.exe "%~dp0..\bin\composer\composer.phar" install
+%~dp0..\bin\php\php-8.0.0-Win32-vs16-x64\php.exe "%~dp0..\bin\composer\composer.phar" upgrade
+%~dp0..\bin\php\php-8.0.0-Win32-vs16-x64\php.exe "%~dp0..\bin\composer\composer.phar" update
+pause
